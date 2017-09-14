5a17ca0a10adbd112e57d118afd11ca6ed629077
David Antliff <david@pitchblende.co.nz>
2017-09-14 22:41:17 +1200

    Use RMT to generate counter-enabled window. WIP.

--- main/app_main.c
+++ main/app_main.c
@@ -28,6 +28,7 @@
 #include "driver/gpio.h"
 #include "driver/pcnt.h"
 #include "soc/rtc.h"
+#include "driver/rmt.h"
 
 #include "esp_log.h"
 #include "sdkconfig.h"
@@ -36,8 +37,13 @@
 
 #define GPIO_LED             (GPIO_NUM_2)
 #define GPIO_FREQ_SIGNAL     (CONFIG_FREQ_SIGNAL_GPIO)
+#define GPIO_RMT             (GPIO_NUM_5)
 
 #define PCNT_UNIT 0
+#define RMT_TX_CHANNEL RMT_CHANNEL_0
+//#define RMT_CLK_DIV          20   // results in 0.25us steps (80MHz / 20 = 4 MHz
+#define RMT_CLK_DIV            160   // results in 2us steps (80MHz / 160 = 0.5 MHz
+//#define RMT_CLK_DIV          1  // results in 25ns steps (80MHz / 2 / 1 = 40 MHz)
 
 //#define ESP_INTR_FLAG_DEFAULT 0
 //
@@ -107,6 +113,76 @@ void read_counter_task(void * arg)
     }
 }
 
+
+
+typedef struct
+{
+    float period;
+} rmt_tx_task_args_t;
+
+void rmt_tx_task(void * arg)
+{
+    rmt_tx_task_args_t * rmt_tx_task_args = arg;
+
+    bool led = false;
+    while (1)
+    {
+        led_set(led);
+        led = !led;
+
+        rmt_item32_t items[100] = { 0 };
+        int num_items = 0;
+
+//        // toggle:
+//        for (num_items = 0; num_items < 16; ++num_items)
+//        {
+//            items[num_items].level0 = 0x1 & num_items;
+//            items[num_items].duration0 = 1;
+//            items[num_items].level1 = ~items[num_items].level0;
+//            items[num_items].duration1 = 1;
+//        }
+
+        // high for exactly x seconds:
+        float x = 1.0;  // seconds
+        int32_t total_duration = (uint32_t)(x / rmt_tx_task_args->period);
+        ESP_LOGI(TAG, "total_duration %d periods", total_duration);
+
+        // max duration per item is 2^15-1 = 32767
+        while (total_duration > 0)
+        {
+            uint32_t duration = total_duration > 32767 ? 32767 : total_duration;
+            items[num_items].level0 = 1;
+            items[num_items].duration0 = duration;
+            total_duration -= duration;
+            ESP_LOGI(TAG, "duration %d", duration);
+
+            if (total_duration > 0)
+            {
+                uint32_t duration = total_duration > 32767 ? 32767 : total_duration;
+                items[num_items].level1 = 1;
+                items[num_items].duration1 = duration;
+                total_duration -= duration;
+            }
+            else
+            {
+                items[num_items].level1 = 0;
+                items[num_items].duration1 = 0;
+            }
+            ESP_LOGI(TAG, "[%d].level0 %d", num_items, items[num_items].level0);
+            ESP_LOGI(TAG, "[%d].duration0 %d", num_items, items[num_items].duration0);
+            ESP_LOGI(TAG, "[%d].level1 %d", num_items, items[num_items].level1);
+            ESP_LOGI(TAG, "[%d].duration1 %d", num_items, items[num_items].duration1);
+
+            ++num_items;
+        }
+        ESP_LOGI(TAG, "num_items %d", num_items);
+
+        rmt_write_items(RMT_TX_CHANNEL, items, num_items, false);
+        rmt_wait_tx_done(RMT_TX_CHANNEL);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
 void app_main()
 {
 //    xSemaphore = xSemaphoreCreateBinary();
@@ -164,9 +240,35 @@ void app_main()
     pcnt_counter_pause(PCNT_UNIT);
     pcnt_counter_clear(PCNT_UNIT);
 
-    xTaskCreate(read_counter_task, "read_counter_task", 2048, NULL, 10, NULL);
+//    xTaskCreate(read_counter_task, "read_counter_task", 2048, NULL, 10, NULL);
 
     pcnt_counter_resume(PCNT_UNIT);
 
+
+    // RMT
+
+    rmt_config_t rmt_tx = {
+        .rmt_mode = RMT_MODE_TX,
+        .channel = RMT_TX_CHANNEL,
+        .gpio_num = GPIO_RMT,
+        .mem_block_num = 1,  // 0 is invalid
+        .clk_div = RMT_CLK_DIV,
+        .tx_config.loop_en = false,
+        .tx_config.carrier_en = false,
+        .tx_config.idle_level = RMT_IDLE_LEVEL_LOW,
+        .tx_config.idle_output_en = true,
+    };
+    rmt_config(&rmt_tx);
+    //rmt_set_source_clk(RMT_TX_CHANNEL, RMT_BASECLK_APB);
+    rmt_driver_install(rmt_tx.channel, 0, 0);
+
+    const float RMT_PERIOD = (float)(RMT_CLK_DIV) / 80000000.0;
+    ESP_LOGI(TAG, "RMT_PERIOD %.10e", RMT_PERIOD);
+    rmt_tx_task_args_t rmt_tx_task_args = {
+        .period = RMT_PERIOD,
+    };
+    xTaskCreate(rmt_tx_task, "rmt_tx_task", 2048, &rmt_tx_task_args, 10, NULL);
+
     ESP_LOGI(TAG, "[APP] Idle.");
+    while(1) ;
 }
